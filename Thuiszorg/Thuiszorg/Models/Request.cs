31b23d82b2c368816e4f3c7f7790283f1e60f768
Stefan van den Berk <stefanvandenberk@hotmail.com>
2013-10-09 15:15:56 +0200

    Request Model Added
    Request Model aangemaakt om taken te maken.

---
+++ Thuiszorg/Thuiszorg/Models/Request.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Thuiszorg.Models
+{
+    public class Request
+    {
+        public int ID { get; set; }
+
+        public string title { get; set; }
+
+        public string description { get; set; }
+
+        public DateTime dateAndTime { get; set; }
+
+        public string user { get; set; }
+
+        public bool completed { get; set; }
+
+        
+    }
+}
