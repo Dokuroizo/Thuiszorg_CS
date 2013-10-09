6243130decaa0f08c2c10e0981870d5dbab692ea
Stefan van den Berk <stefanvandenberk@hotmail.com>
2013-10-09 15:11:28 +0200

    User Model Created
    User Model gemaakt. Basis gegevens van standaard gebruiker gemaakt.

---
+++ Thuiszorg/Thuiszorg/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Thuiszorg.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string passWord { get; set; }
+
+        public string role { get; set; }
+
+        public string email { get; set; }
+
+        public string city { get; set; }
+
+        public string telephone { get; set; }
+    }
+}
